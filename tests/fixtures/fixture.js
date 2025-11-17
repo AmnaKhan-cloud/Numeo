432f3f380f09af6786305c5254656c5e7019c5d1
Amna Khan <amnanazim25@gmail.com>
2025-11-17 10:32:00 +0500

    secret saved in env & open cart

--- tests/fixtures/fixture.js
+++ tests/fixtures/fixture.js
@@ -1,18 +1,19 @@
 import 'dotenv/config';
 import { test as base } from 'playwright-bdd';
+import * as Pages from '../pages/pages.js';
 import { LoginPage } from '../../tests/pages/LoginPage.js';
 import { OpenCartPage } from '../../tests/pages/OpenCartPage.js';
 // helper to create a fixture from a page class
 // Helper to create a page fixture
 const createPageFixture = (PageClass) => async ({ page }, use) => {
   const pageObject = new PageClass(page);
-  await use(pageObject);
+  await use(pageObject);;
 };
 
 // Extend Playwright-BDD test with our page fixtures
-export const test = base.extend({
-  pLoginPage: createPageFixture(LoginPage),
-  pOpenCartPage: createPageFixture(OpenCartPage),
+const test = base.extend({
+  pLoginPage: createPageFixture(Pages.PLoginPage),
+  pOpenCartPage: createPageFixture(Pages.POpenCartPage),
 });
 
-
+export { test };
---
+++ tests/fixtures/pages.js
@@ -0,0 +1,2 @@
+export { PLoginPage } from './LoginPage.js';
+export { POpenCartPage } from './OpenCartPage.js';
